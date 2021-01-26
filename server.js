c9213c42078f6b72b7e34e755834a65f920c3c52
Ross Keeley <rjkeeley@gmail.com>
2021-01-26 16:30:46 +0000

    Created up feature

--- server.js
+++ server.js
@@ -4,67 +4,70 @@ const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 // const app = express()
 var app = express();
-//
-// MongoClient.connect('mongodb+srv://rosskeeley:dude0144Quotes@crud-express-quote-app.yzkex.mongodb.net/crud-express-quote-app?retryWrites=true&w=majority', { useUnifiedTopology: true })
-//   .then(client => {
-//     console.log('Connected to Database')
-//     const db = client.db('crud-express-quote-app');
-//     const quotesCollection = db.collection('quotes')
-//
-//   app.use(bodyParser.urlencoded({
-//     extended: true
-//   }))
-//   app.use(bodyParser.json())
-//   app.use(express.static('public'))
-
-  MongoClient.connect('mongodb+srv://rosskeeley:dude0144Quotes@crud-express-quote-app.yzkex.mongodb.net/crud-express-quote-app?retryWrites=true&w=majority',
-  { useUnifiedTopology: true })
-  .then(client => {
-    console.log('Connected to Database');
-    const db = client.db('crud-express-quote-app');
-    const quotesCollection = db.collection('quotes');
-
-    app.set('view engine', 'ejs');
-
-    app.use(bodyParser.urlencoded({ extended: true }));
-
-    app.get('/', (req, res) => {
-      const cursor = db.collection('quotes').find().toArray()
-      .then(results => {
-        res.render('index.ejs', { quotes: results });
-      })
-      .catch(error => console.error(error));
-    });
-
-    app.post('/quotes', (req, res) => {
-      quotesCollection.insertOne(req.body)
-      .then(result => {
-        res.redirect('/');
-      })
-      .catch(error => console.error(error));
-    });
-
-    app.listen(3000, function() {
-      console.log('Listening on 3000 babaaaay yahaaaaa');
-    });
-
-  })
-  .catch(console.error);
 
 
-//   app.post('/quotes', (req, res) => {
-//     quotesCollection.insertOne(req.body)
-//       .then(result => {
-//         console.log(result)
-//       })
-//       .catch(error => console.error(error))
-//   });
+MongoClient.connect('mongodb+srv://rosskeeley:dude0144Quotes@crud-express-quote-app.yzkex.mongodb.net/crud-express-quote-app?retryWrites=true&w=majority',
+{ useUnifiedTopology: true })
+.then(client => {
+  console.log('Connected to Database');
+  const db = client.db('crud-express-quote-app');
+  const quotesCollection = db.collection('quotes');
 
+  app.set('view engine', 'ejs');
 
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(express.static('public'));
+  // Allows the server to accept JSON data
+  app.use(bodyParser.json());
 
+  app.get('/', (req, res) => {
+    const cursor = db.collection('quotes').find().toArray()
+    .then(results => {
+      res.render('index.ejs', { quotes: results });
+    })
+    .catch(error => console.error(error));
+  });
 
-// }).catch(console.error)
+  app.post('/quotes', (req, res) => {
+    quotesCollection.insertOne(req.body)
+    .then(result => {
+      res.redirect('/');
+    })
+    .catch(error => console.error(error));
+  });
 
+  app.put('/quotes', (req, res) => {
+    // MongoDB Collections method to find and change one item in the database
+    quotesCollection.findOneAndUpdate(
+      // Query - filter the collection with key-value pairs
+      { name: 'Yoda' },
+      // Update - tells mongoDB what to change. Uses mongoDBs update operators like $set, $inc and $push
+      // $set operator sets the new values
+      {
+        $set: {
+          name: req.body.name,
+          quote: req.body.quote
+        }
+      },
+      // Options - tells mongoDB to define additional options for this update request
+      // In this case mongoDB is forced to create the new quote if no quote matching the parameters exists to replace
+      // upsert - insert a document if no document can be updated
+      {
+        upsert: true
+      }
+    )
+    .then(result => {
+      res.json('Success');
+    })
+    .catch(error => console.error(error));
+  });
+
+  app.listen(3000, function() {
+    console.log('Listening on 3000 babaaaay yahaaaaa');
+  });
+
+})
+.catch(console.error);
 
 
 console.log('May Node be with you');
